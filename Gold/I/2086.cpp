9ab06e7edc2462da647f8e7eaa852d7ad6cdd78c
C792 <ians10845@gmail.com>
2022-05-20 23:32:53 +0900

    Gold I

---
+++ Gold/I/2086.cpp
@@ -0,0 +1,50 @@
+//2086 : 피보나치 수의 합
+#include<iostream>
+
+typedef unsigned long long ll;
+
+using namespace std;
+
+constexpr ll re = 1'000'000'000;
+
+struct Det {
+	ll data[2][2];
+};
+
+Det prod(Det a, Det b) {
+	Det c;
+	c.data[0][0] = (a.data[0][0] * b.data[0][0] + a.data[0][1] * b.data[1][0]) % re;
+	c.data[0][1] = (a.data[0][0] * b.data[0][1] + a.data[0][1] * b.data[1][1]) % re;
+	c.data[1][0] = (a.data[1][0] * b.data[0][0] + a.data[1][1] * b.data[1][0]) % re;
+	c.data[1][1] = (a.data[1][0] * b.data[0][1] + a.data[1][1] * b.data[1][1]) % re;
+	return c;
+}
+
+Det divide(Det a, ll n) {
+	if (n > 1) {
+		a = divide(a, n / 2);
+		a = prod(a, a);
+		if (n % 2 == 1) {
+			Det b;
+			b.data[0][0] = 1, b.data[0][1] = 1;
+			b.data[1][0] = 1, b.data[1][1] = 0;
+			a = prod(a, b);
+		}
+	}
+	return a;
+}
+
+ll f(ll x) {
+	Det a;
+	a.data[0][0] = 1, a.data[0][1] = 1;
+	a.data[1][0] = 1, a.data[1][1] = 0;
+	a = divide(a, x);
+	return a.data[0][1];
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	ll a, b;
+	cin >> a >> b;
+	cout << (f(b + 2) - f(a + 1) + re) % re;
+}
